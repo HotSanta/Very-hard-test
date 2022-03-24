5907fd62053d5671babb906e0570768efc057aee
HotSanta <loganrothe1@gmail.com>
2022-03-23 23:21:33 -0700

    Add project files.

---
+++ LogansTest/Form1.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace LogansTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox10 = new System.Windows.Forms.PictureBox();
+            this.pictureBox9 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.button2.Location = new System.Drawing.Point(517, 420);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(546, 404);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "0/3";
+            this.label2.Click += new System.EventHandler(this.label2_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label1.Location = new System.Drawing.Point(444, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 31);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Question 1: 12-5=...";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(549, 386);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Score";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(920, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Time:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label5.Location = new System.Drawing.Point(322, 4);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(506, 31);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Question 2: What is the square root of 8?";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label6.Location = new System.Drawing.Point(322, 7);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(460, 31);
+            this.label6.TabIndex = 28;
+            this.label6.Text = "Question 3: What is 7 multplied by 3?";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label7.Location = new System.Drawing.Point(486, 7);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(171, 31);
+            this.label7.TabIndex = 29;
+            this.label7.Text = "You finished!";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label8.Location = new System.Drawing.Point(495, 38);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 31);
+            this.label8.TabIndex = 30;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label9.Location = new System.Drawing.Point(528, 179);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 31);
+            this.label9.TabIndex = 31;
+            this.label9.Text = "label9";
+            // 
+            // pictureBox10
+            // 
+            this.pictureBox10.Image = global::LogansTest.Properties.Resources.download__1_1;
+            this.pictureBox10.Location = new System.Drawing.Point(450, 211);
+            this.pictureBox10.Name = "pictureBox10";
+            this.pictureBox10.Size = new System.Drawing.Size(236, 172);
+            this.pictureBox10.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox10.TabIndex = 34;
+            this.pictureBox10.TabStop = false;
+            // 
+            // pictureBox9
+            // 
+            this.pictureBox9.Image = global::LogansTest.Properties.Resources.download__4_;
+            this.pictureBox9.Location = new System.Drawing.Point(450, 211);
+            this.pictureBox9.Name = "pictureBox9";
+            this.pictureBox9.Size = new System.Drawing.Size(236, 172);
+            this.pictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox9.TabIndex = 33;
+            this.pictureBox9.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Image = global::LogansTest.Properties.Resources.download1;
+            this.pictureBox7.Location = new System.Drawing.Point(450, 211);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(236, 172);
+            this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox7.TabIndex = 32;
+            this.pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Image = global::LogansTest.Properties.Resources.download__3_;
+            this.pictureBox8.Location = new System.Drawing.Point(159, 38);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(236, 172);
+            this.pictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox8.TabIndex = 27;
+            this.pictureBox8.TabStop = false;
+            this.pictureBox8.Click += new System.EventHandler(this.pictureBox8_Click);
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = global::LogansTest.Properties.Resources.download__2_;
+            this.pictureBox6.Location = new System.Drawing.Point(758, 44);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(236, 172);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 25;
+            this.pictureBox6.TabStop = false;
+            this.pictureBox6.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::LogansTest.Properties.Resources.download;
+            this.pictureBox4.Location = new System.Drawing.Point(159, 38);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(236, 172);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 23;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::LogansTest.Properties.Resources.download__1_;
+            this.pictureBox5.Location = new System.Drawing.Point(758, 44);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(236, 172);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 22;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LogansTest.Properties.Resources.Capture;
+            this.pictureBox1.Location = new System.Drawing.Point(401, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(351, 376);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::LogansTest.Properties.Resources.images;
+            this.pictureBox3.Location = new System.Drawing.Point(758, 44);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(236, 172);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 14;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LogansTest.Properties.Resources.images__1_;
+            this.pictureBox2.Location = new System.Drawing.Point(159, 38);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(236, 172);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.button1.Location = new System.Drawing.Point(501, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 81);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1145, 479);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox10);
+            this.Controls.Add(this.pictureBox9);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox10)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox9;
+        private System.Windows.Forms.PictureBox pictureBox10;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ LogansTest/Form1.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LogansTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int score=0;
+        int count = 0;
+        int loss = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            label1.Visible = false;
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            button2.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            label5.Visible = false;
+            pictureBox6.Visible = false;
+            pictureBox8.Visible = false;
+            label6.Visible = false;
+            label7.Visible = false;
+            label9.Visible = false;
+            pictureBox7.Visible = false;
+            pictureBox9.Visible = false;
+            pictureBox10.Visible = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            
+            pictureBox3.Visible = false;
+            pictureBox2.Visible = false;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            score += 1;
+            pictureBox3.Visible=false;
+            pictureBox2.Visible = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        
+
+        
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            score = 0;
+            count = 0;
+            label2.Text = "0/3";
+            label2.Visible = false;
+            label3.Visible = false;
+            label4.Visible = false;
+            button2.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            label1.Visible = false;
+            pictureBox1.Visible = true;
+            button1.Visible = true;
+            pictureBox7.Visible = false;
+            pictureBox9.Visible = false;
+            pictureBox10.Visible = false;
+            label3.Visible = true;
+            label7.Visible = false;
+
+            label8.Visible = false;
+
+            label9.Visible = false;
+            pictureBox7.Visible = false;
+            pictureBox9.Visible = false;
+            pictureBox10.Visible = false;
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox3_Click_1(object sender, EventArgs e)
+        {
+            score += 1;
+            pictureBox3.Visible = false;
+            pictureBox2.Visible = false;
+            string output = score.ToString();
+            label2.Text = (output + "/3");
+            pictureBox4.Visible = true;
+            pictureBox5.Visible = true;
+            label5.Visible = true;
+            label1.Visible = false;
+        }
+
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+            pictureBox3.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox5.Visible = true;
+            pictureBox4.Visible = true;
+            label5.Visible=true;
+            string output=score.ToString();
+            label2.Text = (output + "/3");
+            loss -= 1;
+            label1.Visible = false;
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+            String output = score.ToString();
+            label2.Text = output + "/3";
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            count++;
+            string output = count.ToString();
+            label4.Text = ("Time:"+output);
+            if (count==20)
+            {
+                timer1.Stop();
+                pictureBox2.Visible = false;
+                pictureBox1.Visible = true;
+                button1.Visible = true;
+                pictureBox3.Visible = false;
+                label1.Visible = false;
+                label2.Visible = false;
+                label3.Visible = false;
+                label4.Visible = false;
+                button2.Visible = false;
+                pictureBox4.Visible = false;
+                pictureBox5.Visible = false;
+                label5.Visible = false;
+                pictureBox6.Visible = false;
+                pictureBox8.Visible = false;
+                label6.Visible = false;
+                label7.Visible = false;
+                label9.Visible = false;
+                pictureBox7.Visible = false;
+                pictureBox9.Visible = false;
+                pictureBox10.Visible = false;
+                MessageBox.Show("You Lost. How are that slow?");
+                
+
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            score += 1;
+            string output = score.ToString();
+            label2.Text = (output + "/3");
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            label5.Visible = false;
+            pictureBox6.Visible = true;
+            pictureBox8.Visible = true;
+            label6.Visible = true;
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            loss -= 1;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+            label5.Visible = false;
+            pictureBox6.Visible = true;
+            pictureBox8.Visible = true;
+            label6.Visible = true;
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+            score += 1;
+            string output = score.ToString();
+            label2.Text = (output + "/3");
+            pictureBox6.Visible = false;
+            pictureBox8.Visible = false;
+            label6.Visible = false;
+            label7.Visible = true;
+            label8.Visible = true;
+            label2.Visible = false;
+            label3.Visible = false;
+
+            string output1 = loss.ToString();
+            if (score == 3)
+            {
+                label8.Text = ("Great Job!");
+                pictureBox10.Visible = true;
+            }
+            else if (score == 2)
+            {
+                label8.Text = ("You did okay");
+                pictureBox9.Visible = true;
+            }
+            else
+            {
+
+                label8.Text = ("You did horrible. Won't lie");
+                pictureBox9.Visible = true;
+            }
+            if (loss < 2)
+                label9.Text = ("You only got " + output1 + " wrong and " + output + " right!");
+            else label9.Text = ("You got" + output + " right!");
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+            loss -= 1;
+            pictureBox6.Visible = false;
+            pictureBox8.Visible = false;
+            label6.Visible = false;
+            label7.Visible = true;
+            label8.Visible = true;
+            label2.Visible = false;
+            label3.Visible = false;
+            string output = score.ToString();
+            string output1 = loss.ToString();
+            if (score==3)
+            {
+                label8.Text = ("Great Job!");
+                pictureBox10.Visible = true;
+            }
+            else if (score==2)
+            {
+                label8.Text = ("You did okay");
+                pictureBox9.Visible = true;
+            }
+            else
+            {
+
+                label8.Text = ("You did horrible. Won't lie");
+                pictureBox7.Visible= true;
+            }
+            if (loss < 2)
+                label9.Text = ("You only got " + output1 + " wrong and " + output + " right!");
+            else label9.Text = ("You got" + output + " right!");
+
+
+
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = false;
+            timer1.Start();
+            button1.Visible = false;
+            pictureBox2.Visible = true;
+            pictureBox3.Visible = true;
+            label1.Visible = true;
+            label2.Visible = true;
+            label3.Visible = true;
+            label4.Visible = true;
+            button2.Visible = true;
+            label6.Visible = false;
+            label7.Visible = false;
+            label9.Visible = false;
+        }
+    }
+}
